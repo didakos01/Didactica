b6cc7847b5fd64efbdb64e765ed7b2b9e33e0fe6
DiegoVas01 <dvaspa2001@gmail.com>
2026-06-03 10:07:46 -0600

    Ejemplo segundo básico A 030626

---
+++ Python/Segundo Basico A/condicionales_segundoA.py
@@ -0,0 +1,12 @@
+#condicionales en Python
+num=int(input("Ingrese un número: "))
+
+if num>0:
+    print("El número es positivo")
+else:
+    print("El número es negativo")
+
+if num%2==0:
+    print("El número es par")
+else:
+    print("El número es impar")
